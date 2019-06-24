6cf974a5025cad965e2a048b17d717fe684729d6
Portno <j.remoundos@dotbydot.gr>
2019-06-24 13:06:33 +0300

    nuget + font size

---
+++ src/Panacea.Modules.MediaPlayerContainer/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+
+namespace Panacea.Modules.MediaPlayerContainer
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public App()
+        {
+
+        }
+    }
+}
+
